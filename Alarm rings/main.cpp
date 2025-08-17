77f3af7ee2cca032fa692507a18c72122cfb7288
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 23:20:05 +0530

    wake up

--- Alarm rings/main.cpp
+++ Alarm rings/main.cpp
@@ -9,7 +9,15 @@ int main() {
     while (true) {
         cout << "\nAlarm rings!" << endl;
  
-        
+        // Step 1: Check if ready to wake up
+        cout << "Are you ready to wake up? (y/n): ";
+        cin >> ready;
+ 
+        if (ready == 'y' || ready == 'Y') {
+            cout << "You leave the bed." << endl;
+            cout << "Alarm turned off." << endl;
+            break; // End program
+        }
     }
  
     cout << "\n=== Program Ended ===" << endl;
