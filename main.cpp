4c155df5004880463823183a7cd32395094364c6
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 21:20:19 +0530

    add latte

--- main.cpp
+++ main.cpp
@@ -17,7 +17,18 @@ int main() {
         powerOn = true;
         cout << "\nMachine Powered ON ✅" << endl;
  
-       
+       // Loop until user powers off
+        while (powerOn) {
+            // Coffee Selection
+            cout << "\nSelect Coffee (Latte / Mocha / Espresso): ";
+            cin >> choice;
+ 
+            if (choice == "Latte" || choice == "latte") {
+                cout << "\nPreparing Latte..." << endl;
+                cout << "Adding frothed milk..." << endl;
+                cout << "Adding hot coffee..." << endl;
+            }
+        }
     }
     else {
         cout << "\nMachine not powered ON. Exiting..." << endl;
