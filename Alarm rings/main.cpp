9144f900538c6d4789660cd3b292dfd049de8d74
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-17 23:21:06 +0530

    final

--- Alarm rings/main.cpp
+++ Alarm rings/main.cpp
@@ -18,6 +18,19 @@ int main() {
             cout << "Alarm turned off." << endl;
             break; // End program
         }
+        // Step 2: If not ready, check if late
+        cout << "Will you be late? (y/n): ";
+        cin >> late;
+ 
+        if (late == 'y' || late == 'Y') {
+            cout << "You leave the bed." << endl;
+            cout << "Alarm turned off." << endl;
+            break; // End program
+        } else {
+            cout << "Snoozing alarm for 5 minutes..." << endl;
+            cout << "Waiting... Alarm will ring again!" << endl;
+            // Loop continues, alarm rings again
+        }
     }
  
     cout << "\n=== Program Ended ===" << endl;
