2abd56f5bae599fbc585c5ad75e99eb645a8fc88
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 12:52:01 +0530

    regular hire

--- Hiring/main.cpp
+++ Hiring/main.cpp
@@ -10,5 +10,14 @@ int main() {
     // Ask about regular hiring
     cout << "Is it regular hiring process? (y/n): ";
     cin >> choice;
+    if (choice == 'n' || choice == 'N') {
+        cout << "Special hiring process\n";
+        cout << "End of program\n";
+        return 0;
+    }
+ 
+    // -------- Suitable Candidate Check Sequence --------
+    cout << "Is there a suitable candidate? (y/n): ";
+    cin >> choice;
     return 0;
 }
