b9741a541302d6b1f54b0e047d6c1b69b6ae7156
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 07:51:33 +0530

    first

---
+++ Login/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string correctUser = "admin";     // Predefined username
+    string correctPass = "12345";     // Predefined password
+    string user, pass;
+    int attempts;
+ 
+    cout << "Enter number of attempts already made: ";
+    cin >> attempts;
+ 
+    // Step 1: Check if user already has 5 attempts
+    if (attempts >= 5) {
+        cout << "\nAccount locked due to 5 failed attempts!" << endl;
+        cout << "Alerting user..." << endl;
+    } else {
+        // Step 2: Allow login
+        cout << "\nEnter username: ";
+        cin >> user;
+        cout << "Enter password: ";
+        cin >> pass;
+ 
+       
+    }
+ 
+    cout << "---- End ----" << endl;
+    return 0;
+}
+ 
