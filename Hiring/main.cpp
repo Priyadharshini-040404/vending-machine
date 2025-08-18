147190b1983f6744dad686b5ccf4df59f6799fd4
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 12:50:59 +0530

    first

---
+++ Hiring/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    cout << "Job is defined\n";
+    cout << "Crew request sent to HR\n";
+ 
+    char choice;
+ 
+    // Ask about regular hiring
+    cout << "Is it regular hiring process? (y/n): ";
+    cin >> choice;
+    return 0;
+}
