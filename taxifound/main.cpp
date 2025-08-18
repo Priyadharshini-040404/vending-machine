31da4831f1798ddec9f26cd8ee49ffc198e0554a
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 14:29:31 +0530

    first

---
+++ taxifound/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+ 
+int main() {
+    cout << "=== Ride Assignment Program ===\n";
+ 
+    // Step 1: How was the order placed
+    char method;
+    cout << "Order placed by (P)hone or (S)oftware? ";
+    cin >> method;
+
+    return 0;
+}
