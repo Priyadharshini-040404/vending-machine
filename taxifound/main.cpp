273508147e45f5abefe9da6c798a90d84785a3f0
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 14:30:42 +0530

    request

--- taxifound/main.cpp
+++ taxifound/main.cpp
@@ -10,6 +10,13 @@ int main() {
     char method;
     cout << "Order placed by (P)hone or (S)oftware? ";
     cin >> method;
-
+    if (method == 'P' || method == 'p')
+        cout << "Sending request to Call Centre Server...\n";
+    else if (method == 'S' || method == 's')
+        cout << "Sending request to Software Server...\n";
+    else {
+        cout << "Invalid option. Exiting...\n";
+        return 0;
+    }
     return 0;
 }
