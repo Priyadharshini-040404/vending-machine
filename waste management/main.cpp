ded8ac86e4d9c0e6d69a902caaf867646476159d
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 07:38:30 +0530

    first

---
+++ waste management/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string wasteType;
+    char choice;
+ 
+    cout << "---- Waste Processing System ----" << endl;
+ 
+    // Step 1: Get Waste
+    cout << "Enter waste type (organic / others): ";
+    cin >> wasteType;
+ 
+    if (wasteType == "organic") {
+        // Step 2: Check High Energy
+        cout << "Does it have high energy? (y/n): ";
+        cin >> choice;
+ 
+        if (choice == 'y' || choice == 'Y') {
+            cout << "Process output: Bio-fuel" << endl;
+        } else {
+            cout << "Process output: Organic fertilizer" << endl;
+        }
+    } else {
+        // Step 3: Check Reusable
+        cout << "Is it reusable? (y/n): ";
+        cin >> choice;
+ 
+        
+    }
+ 
+    cout << "---- Processing Completed ----" << endl;
+    return 0;
+}
