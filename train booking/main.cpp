a26b4996936fbd8141ab70e655b62a56e241559f
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 15:08:47 +0530

    second

--- train booking/main.cpp
+++ train booking/main.cpp
@@ -9,6 +9,13 @@ bool getUserConfirmation(const string& promptMessage) {
     cin >> userInput;
     return (userInput == 'y' || userInput == 'Y');
 }
+void handleTicketCollection(const string& collectionMethod) {
+    if (collectionMethod == "machine") {
+        cout << "Collect your ticket from the machine.\n";
+    } else if (collectionMethod == "mail") {
+        cout << "Check the letter box in 3 days for your ticket.\n";
+    }
+}
 int main() {
     cout << "Starting Train Journey Reservation System\n";
     bool reservationStatus = processReservation();
