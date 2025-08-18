4ff6edf7d3c59fec34ebcd28fa3bf86451acff54
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-08-18 14:35:49 +0530

    step2

--- taxifound/main.cpp
+++ taxifound/main.cpp
@@ -18,5 +18,23 @@ int main() {
         cout << "Invalid option. Exiting...\n";
         return 0;
     }
+    / Step 2: Check pickup time
+    bool pickupWithin30 = false;
+    while (!pickupWithin30) {
+        // (Here we just simulate the condition check)
+        cout << "Checking if pickup is within 30 minutes...\n";
+ 
+        // For demo: press y/n to simulate condition
+        char ans;
+        cout << "Is pickup within 30 mins? (y/n): ";
+        cin >> ans;
+ 
+        if (ans == 'y' || ans == 'Y') {
+            pickupWithin30 = true;
+        } else {
+            cout << "Not within 30 mins. Wait 1 minute and check again...\n";
+            this_thread::sleep_for(chrono::seconds(1)); // simulate 1 min as 1 sec
+        }
+    }
     return 0;
 }
